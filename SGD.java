bc8b7c7928cf1358ff36792b528987e48745632e
Jason Whitmore <jasonwhitmore12@gmail.com>
2020-10-17 23:52:15 -0700

    docs for SGD

--- SGD.java
+++ SGD.java
@@ -1,18 +1,41 @@
 import java.util.ArrayList;
 
+
+/**
+ * Class for the classic Stochastic Gradient Descent optimizer which performs
+ * no processing to the input gradients.
+ */
 public class SGD implements Optimizer{
 
+    /**
+     * The scalar to multiply the gradient by before applying to the model's parameters.
+     * Should be greater than 0.
+     */
     private float learningRate;
 
+    /**
+     * Default constructor for SGD. Sets a learning rate of 0.0001.
+     */
     public SGD(){
         learningRate = 0.0001f;
     }
 
+    /**
+     * Constructor for SGD that uses a user defined learning rate
+     * @param learningRate The scalar to multiply the gradient by.
+     * Smaller values converge slowly, but with more stability.
+     */
     public SGD(float learningRate){
         this.learningRate = learningRate;
     }
 
 
+    /**
+     * Performs no adjustment to the raw gradient other than the typical scaling
+     * by the learning rate
+     * @param rawGradient A raw, unprocessed gradient of the model's parameters evaluated at a data point.
+     * @return The processed gradient, ready to be applied to the model.
+     */
     @Override
     public ArrayList<float[][]> processGradient(ArrayList<float[][]> rawGradient){
         Utility.scaleGradient(rawGradient, this.learningRate);
