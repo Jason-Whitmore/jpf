c6b24ad8a2aa33f68d0b2204b2c3d5b7c11eb0c2
Jason Whitmore <jasonwhitmore12@gmail.com>
2020-10-17 23:43:25 -0700

    docs for the optimizer interface

--- Optimizer.java
+++ Optimizer.java
@@ -1,5 +1,15 @@
 import java.util.ArrayList;
 
+/**
+ * Interface used by all optimizers that enforces the implementation of the processGradient() function
+ */
 public interface Optimizer {
+    
+    /**
+     * Processes a raw gradient, possibly performs an adjustment based on an optimizer state,
+     * the returns a gradient to be applied to the model's parameters.
+     * @param rawGradient The raw, unprocessed gradient for the model evaluated at a data point
+     * @return The processed gradient
+     */
     public ArrayList<float[][]> processGradient(ArrayList<float[][]> rawGradient);
 }
