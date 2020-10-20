5b99cd75f7484acac2169214dd06f23f2f4a7a3f
Jason Whitmore <jasonwhitmore12@gmail.com>
2020-10-19 21:12:37 -0700

    docs for loss

--- Loss.java
+++ Loss.java
@@ -1,7 +1,30 @@
+
+/**
+ * Interface that defines the functions used for loss functions
+ */
 public interface Loss{
-    public float[] lossVector(float[] yTrue, float[] yPredicted);
 
-    public float[] lossVectorGradient(float[] yTrue, float[] yPredicted);
+    /**
+     * Calculates the loss for each component in the output vector
+     * @param yTrue The ground truth, or training output vector
+     * @param yPredicted The predicted output vector from a model.
+     * @return The loss calculation for each component in the output vector
+     */
+    public float[] calculateLossVector(float[] yTrue, float[] yPredicted);
 
+    /**
+     * Calculates the gradient (direction of steepest ascent) of the loss function
+     * @param yTrue The ground truth, or training output vector
+     * @param yPredicted The predicted output vector from a model.
+     * @return The gradient vector for each component in the output vector.
+     */
+    public float[] calculateLossVectorGradient(float[] yTrue, float[] yPredicted);
+
+    /**
+     * Calculates the scalar loss.
+     * @param yTrue The ground truth, or training output vector.
+     * @param yPredicted The predicted output vector from a model.
+     * @return The scalar loss.
+     */
     public float calculateLossScalar(float[] yTrue, float[] yPredicted);
 }
