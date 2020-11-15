c5d14dbf4f1ced7710225237f048a9267c41807a
Jason Whitmore <jasonwhitmore12@gmail.com>
2020-11-14 23:52:14 -0800

    completing constructors for RMSProp

--- RMSProp.java
+++ RMSProp.java
@@ -1,5 +1,4 @@
-
-
+import java.util.ArrayList;
 
 public class RMSProp implements Optimizer{
 
@@ -12,6 +11,23 @@ public class RMSProp implements Optimizer{
     ArrayList<float[][]> state;
 
     public RMSProp(){
+        learningRate = 0.0001f;
 
+        rho = 0.9f;
+
+        epsilon = 0.0000001f;
+    }
+
+    public RMSProp(float learningRate, float rho, float epsilon){
+        this.learningRate = learningRate;
+
+        this.rho = rho;
+
+        this.epsilon = epsilon;
+    }
+
+
+    public ArrayList<float[][]> processGradient(ArrayList<float[][]> rawGradient){
+        return null;
     }
 }
