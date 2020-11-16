5cb9c53549e26ff758c7b98861dbce592a717d4f
Jason Whitmore <jasonwhitmore12@gmail.com>
2020-11-15 23:21:11 -0800

    completed reset state for RMSprop

--- RMSProp.java
+++ RMSProp.java
@@ -32,15 +32,27 @@ public class RMSProp implements Optimizer{
 
 
     public ArrayList<float[][]> processGradient(ArrayList<float[][]> rawGradient){
-        return null;
+        if(prevGradSquare == null){
+            ArrayList<float[][]> newState = Utility.cloneArrays(rawGradient);
+            Utility.clearArrays(newState);
+        }
+
+        ArrayList<float[][]> grad = Utility.cloneArrays(rawGradient);
+        Utility.clearArrays(grad);
+
+        
     }
 
+    /**
+     * Resets the optimizer state. Useful when retraining a model on new data.
+     */
     public void resetState(){
         if(prevGradSquare == null){
             return;
         }
 
         ArrayList<float[][]> newState = Utility.cloneArrays(prevGradSquare);
+        Utility.clearArrays(newState);
 
         prevGradSquare = newState;
     }
