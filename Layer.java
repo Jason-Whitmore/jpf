d196ab5495f5822af3811c3ec20bcf051b2fe645
Jason Whitmore <jasonwhitmore12@gmail.com>
2020-12-01 23:13:15 -0800

    adding getters/setters for Layer class

--- Layer.java
+++ Layer.java
@@ -7,18 +7,44 @@ public abstract class Layer {
 
     protected ArrayList<Layer> inputLayers;
 
-    protected ArrayList<float[]> inputVectors;
 
 
 
     protected ArrayList<Layer> outputLayers;
 
-    protected ArrayList<float[]> outputVectors;
 
+
+    //getters and setters for class fields
+
+    public ArrayList<float[][]> getParameters(){
+        return parameters;
+    }
+
+
+    public void setParameters(ArrayList<float[][]> params){
+        parameters = params;
+    }
+
+
+    public ArrayList<Layer> getInputLayers(){
+        return inputLayers;
+    }
+
+    public void setInputLayers(ArrayList<Layer> layers){
+        inputLayers = layers;
+    }
+
+
+    public ArrayList<Layer> getOutputLayers(){
+        return outputLayers;
+    }
+
+    public void setOutputLayers(ArrayList<Layer> layers){
+        outputLayers = layers;
+    }
 
     
     public abstract void forwardPass();
-
 
     public abstract void backwardPass();
 
