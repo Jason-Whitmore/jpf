98ca120fdf7618cbb61701a6471fe81c1064fd4c
Jason Whitmore <jasonwhitmore12@gmail.com>
2020-12-03 23:51:23 -0800

    adding tanh activation function

---
+++ Tanh.java
@@ -0,0 +1,15 @@
+
+
+
+public class Tanh extends ActivationFunction{
+
+    public float f(float x){
+        return (float)Math.tanh(x);
+    }
+
+    public float fPrime(float x){
+        float t = (float)Math.tanh(x);
+
+        return 1 - (t * t);
+    }
+}
