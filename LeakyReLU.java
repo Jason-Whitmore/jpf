df0673f926227c751d53b1c55c5f69c1898f21fc
Jason Whitmore <jasonwhitmore12@gmail.com>
2021-02-26 19:50:48 -0800

    adding new LeakyRelu activation function

---
+++ LeakyReLU.java
@@ -0,0 +1,27 @@
+
+
+
+public class LeakyReLU extends ActivationFunction{
+
+    private float alpha;
+
+    public LeakyReLU(float alpha){
+        this.alpha = alpha;
+    }
+
+    public float f(float x){
+        if(x > 0){
+            return x;
+        } else {
+            return x * alpha;
+        }
+    }
+
+    public float fPrime(float x){
+        if(x > 0){
+            return 1;
+        } else {
+            return alpha;
+        }
+    }
+}
