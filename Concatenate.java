32ee60ce247d0d10e6a4a8c36a8351dff2b7ab77
Jason Whitmore <jasonwhitmore12@gmail.com>
2021-03-22 22:59:07 -0700

    completed forward pass code on concatenate

--- Concatenate.java
+++ Concatenate.java
@@ -5,28 +5,42 @@ public class Concatenate extends Layer{
 
     private int size;
 
-    private ArrayList<Layer> inputLayers;
-
     public Concatenate(ArrayList<Layer> inputLayers){
+        super();
+
         this.inputLayers = inputLayers;
+
         updateLayerSize();
+        outputVector = new float[this.size];
     }
 
     public Concatenate(Layer[] inputLayers){
+        super();
+
         ArrayList<Layer> inputLayersList = new ArrayList<Layer>(inputLayers.length);
 
         Collections.addAll(inputLayersList, inputLayers);
-
         this.inputLayers = inputLayersList;
+
         updateLayerSize();
+
+        outputVector = new float[this.size];
     }
 
     public void forwardPass(){
 
+        int outputIndex = 0;
+
+        for(int i = 0; i < inputLayers.size(); i++){
+            for(int j = 0; j < inputLayers.get(i).inputVector.length; j++){
+                outputVector[outputIndex] = inputLayers.get(i).inputVector[j];
+                outputIndex++;
+            }
+        }
     }
 
     public void backwardPass(){
-        
+
     }
 
 
