aaaf40bd6a79a3196f75940e8446427a3c9a9653
Jason Whitmore <jasonwhitmore12@gmail.com>
2021-03-30 23:00:47 -0700

    doing softmax backprop

--- Softmax.java
+++ Softmax.java
@@ -13,6 +13,8 @@ public class Softmax extends Layer{
         this.inputVector = new float[this.numUnits];
         this.outputVector = new float[this.numUnits];
 
+        this.dLdX = new float[this.numUnits];
+        this.dLdY = new float[this.numUnits];
 
     }
 
@@ -31,6 +33,31 @@ public class Softmax extends Layer{
     }
 
     public void backwardPass(){
+        //Determine error vector from next layers
+
+        for(int i = 0; i < getOutputLayers().size(); i++){
+            for(int j = 0; j < getOutputLayers().get(i).getdLdX().length; j++){
+                getdLdY()[j] += getOutputLayers().get(i).getdLdX()[j];
+            }
+        }
+
+        for(int i = 0; i < this.inputVector.length; i++){
+
+            //Determine the sum of exponents that are not x[i]
+            float constant = 0;
+            for(int j = 0; j < this.inputVector.length; j++){
+                if(j != i){
+                    constant += (float)Math.exp((double)this.inputVector[j]);
+                }
+            }
+
+            double exponential = Math.exp((double)this.inputVector[i]);
+
+            //gradient from the calculus quotient rule
+            double gradient = (((exponential + constant) * exponential) - (exponential * exponential)) / (float)Math.pow((double)exponential, 2.0);
+
+            this.dLdX[i] = (float)gradient;
+        }
 
     }
 }
