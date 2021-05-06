6d62bfb66f6a88b18fbad2b554afa80d5ce7963b
Jason Whitmore <jasonwhitmore12@gmail.com>
2021-05-05 22:08:57 -0700

    working on linear model example

--- Examples.java
+++ Examples.java
@@ -5,9 +5,49 @@ public class Examples{
     private static final String OPTION_STRING = "";
 
     
-    private void LinearExample(){
+    private void SimpleLinear(){
+        System.out.println("Starting Simple Linear Model example: Fitting model to f(x) = 2x - 1");
 
-        //Creating dataset
+        //Creating dataset for f(x) = 2x - 1
+        System.out.println("Creating dataset...");
+
+        int numSamples = 1000;
+        float[][] trainingInputs = new float[numSamples][];
+        float[][] trainingOutputs = new float[numSamples][];
+
+        for(int i = 0; i < numSamples; i++){
+            float[] x = new float[1];
+            x[0] = Utility.getRandomUniform(-10f, 10f);
+
+            float[] y = new float[1];
+            y[0] = (2 * x[0]) - 1;
+
+            trainingInputs[i] = x;
+            trainingOutputs[i] = y;
+        }
+
+        //Create the model
+        System.out.println("Creating the linear model...");
+
+        LinearModel model = new LinearModel(1, 1);
+
+        //Train the model
+        System.out.println("Training the linear model...");
+
+        float untrainedLoss = model.calculateLoss(trainingInputs, trainingOutputs, new MSE());
+
+        model.fit(trainingInputs, trainingOutputs, 100, 32, 0.01f, new SGD(0.01f), new MSE());
+
+        //Calculate the loss
+
+        float trainedLoss = model.calculateLoss(trainingInputs, trainingOutputs, new MSE());
+
+        //Print out training loss progress
+        System.out.println("Training complete.");
+        System.out.println("Loss before training: " + untrainedLoss);
+        System.out.println("Loss after training: " + trainedLoss);
+
+        
     }
 
     public static void main(String[] args){
