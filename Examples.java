24d8af45753cec6324305ed8eb83fad59ab34169
Jason Whitmore <jasonwhitmore12@gmail.com>
2021-05-05 21:43:19 -0700

    working on an examples demo file

---
+++ Examples.java
@@ -0,0 +1,20 @@
+
+
+public class Examples{
+
+    private static final String OPTION_STRING = "";
+
+    
+    private void LinearExample(){
+
+        //Creating dataset
+    }
+
+    public static void main(String[] args){
+
+        if(args.length == 0){
+            System.out.println(OPTION_STRING);
+        }
+
+    }
+}
