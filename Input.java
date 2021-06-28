3ed664af5fe2de62f3766db7884b25f91e107506
Jason Whitmore <jasonwhitmore12@gmail.com>
2021-06-28 15:58:04 -0700

    finishing Input.java docs/other syntax fixes

--- Input.java
+++ Input.java
@@ -1,14 +1,27 @@
 
+/**
+ * Defines the Input layer class which is a concrete derived class of the Layer abstract class.
+ * The Input layer is used as the first layers in a NeuralNetwork model.
+ */
 public class Input extends Layer{
 
+    /**
+     * Basic constructor for the Input layer object which is used as the first layers
+     * in a NeuralNetwork model.
+     * @param inputVectorSize The length of the input vector for this layer. Should be >= 1.
+     */
     public Input(int inputVectorSize){
         super();
 
-        inputVector = new float[inputVectorSize];
-
-        outputVector = new float[inputVectorSize];
+        this.inputVector = new float[inputVectorSize];
+        this.outputVector = new float[inputVectorSize];
     }
 
+    /**
+     * Constructs an Input layer object from the string produced by the toString method of this class.
+     * @param layerInfoString The layer info string produced by toString. The syntax of this string is simple:
+     * The string "INPUT(5)" defines an Input layer whose input vector is of length 5.
+     */
     public Input(String layerInfoString){
         super();
 
@@ -16,20 +29,20 @@ public class Input extends Layer{
 
         int layerSize = Integer.parseInt(layerSizeString);
 
-        inputVector = new float[layerSize];
-        outputVector = new float[layerSize];
+        this.inputVector = new float[layerSize];
+        this.outputVector = new float[layerSize];
     }
 
 
 
     public void forwardPass(){
         //copy data directly from input vector to output vector
-        Utility.copyArrayContents(getInputVector(), getOutputVector());
+        Utility.copyArrayContents(this.inputVector, this.outputVector);
 
-        for(int i = 0; i < outputLayers.size(); i++){
-            float[] nextInputVector = outputLayers.get(i).getInputVector();
+        for(int i = 0; i < this.outputLayers.size(); i++){
+            float[] nextInputVector = this.outputLayers.get(i).getInputVector();
 
-            Utility.copyArrayContents(outputVector, nextInputVector);
+            Utility.copyArrayContents(this.outputVector, nextInputVector);
         }
     }
 
