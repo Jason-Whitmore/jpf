dfe95dbc219d6b51bdfc6f656e21a05a8202d418
Jason Whitmore <jasonwhitmore12@gmail.com>
2021-06-29 17:36:50 -0700

    documenting more, including csvwriter class

--- CSVWriter.java
+++ CSVWriter.java
@@ -1,14 +1,31 @@
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * Class definition for CSVWriter, which allows users to easily write data to .csv files to be opened in other programs.
+ */
 public class CSVWriter {
 
+    /**
+     * 
+     */
     private String dataString;
 
+    /**
+     * The filepath where the csv file will be written to
+     */
     private String filePath;
 
+    /**
+     * The number of columns in the csv file
+     */
     private int numCols;
 
+    /**
+     * Constructs the CSVWriter object with user defined parameters.
+     * @param filePath The filepath where the csv file will be written to.
+     * @param columnHeaders The names for each of the columns which will show up as the first row in the csv file.
+     */
     public CSVWriter(String filePath, String[] columnHeaders){
         this.filePath = filePath;
 
@@ -19,6 +36,11 @@ public class CSVWriter {
         addRow(columnHeaders);
     }
 
+    /**
+     * Adds a new row to the csv, but does not write the new row to disk.
+     * @param newRow The new row. Should be the same size as the column header array
+     * used to create the CSVWriter object.
+     */
     public void addRow(String[] newRow){
         for(int i = 0; i < newRow.length - 1; i++){
             dataString += newRow[i] + ", ";
@@ -27,6 +49,10 @@ public class CSVWriter {
         dataString += newRow[newRow.length - 1] + "\n";
     }
 
+    /**
+     * Writes the csv file stored in memory to the filepath on disk.
+     * May display error message on failure.
+     */
     public void writeToFile(){
 
         try{
--- CrossEntropy.java
+++ CrossEntropy.java
@@ -1,12 +1,22 @@
-
-
-
+/**
+ * Class that defines the CrossEntropy loss function, which is a concrete class derived from the Loss abstract class.
+ * This loss is used for classification tasks 
+ */
 public class CrossEntropy implements Loss{
 
+    /**
+     * Constructor that simply initializes the loss function object.
+     */
     public CrossEntropy(){
 
     }
 
+    /**
+     * The cross entropy loss function for single variable input and output
+     * @param yTrue The label variable, which should be either 1 or 0
+     * @param yPredicted The predicted output from a model, should be in range (0, 1)
+     * @return The scalar loss
+     */
     private float CELoss(float yTrue, float yPredicted){
         if(yTrue == 1){
             return (float)(-Math.log((yPredicted)));
@@ -15,9 +25,11 @@ public class CrossEntropy implements Loss{
         }
     }
 
+    //TODO: complete this
     private float CELossPrime(float yTrue, float yPredicted){
         return 0;
     }
+
 
     public float[] calculateLossVector(float[] yTrue, float[] yPredicted){
         float[] r = new float[yTrue.length];
