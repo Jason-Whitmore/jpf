49e3a60aaf54781051ccf2c352ce04790ac0220c
Jason Whitmore <jasonwhitmore12@gmail.com>
2021-07-02 20:13:31 -0700

    editing activation function docs to reflect function outputs on tanh and sigmoid

--- Sigmoid.java
+++ Sigmoid.java
@@ -12,6 +12,11 @@ public class Sigmoid extends ActivationFunction{
 
     }
 
+    /**
+     * The main activation function. sigmoid(x) outputs numbers in range (0, 1).
+     * @param x The input number.
+     * @return The output number.
+     */
     public float f(float x){
         float xExp = (float)(Math.exp((double)x));
 
--- Tanh.java
+++ Tanh.java
@@ -1,8 +1,14 @@
-
-
-
+/**
+ * Defines the Tanh (hyperbolic tangent) activation function which is derived
+ * from the abstract ActivationFunction class.
+ */
 public class Tanh extends ActivationFunction{
 
+    /**
+     * The main activation function. Tanh(x) outputs numbers in range (-1, 1).
+     * @param x The input number.
+     * @return The output number.
+     */
     public float f(float x){
         return (float)Math.tanh(x);
     }
